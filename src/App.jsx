5e22286f627893fd76db419523e1a19497de65e3
tianahR <farahrabenjam@yahoo.com>
2024-12-27 23:32:40 -0500

    Display Posts from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,83 @@
 import React, { useState, useEffect } from "react";
 // import { getAllData } from './util/index';
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
 } from "react-router-dom";
 import Header from "./components/header/Header"; // New Header
 import "./index.css";
 import Login from "./pages/forms/Login";
 import Home from "./pages/Home/home";
 import Profile from "./pages/profile/Profile";
-import Register from './pages/forms/Register';
-import PostsPage from './pages/posts-page/postsPage';
-import CreatePost from './pages/create-post/CreatePost';
-import AdminDashboard from './pages/Admin/AdminDashboard';
-import UpdateProfile from './pages/profile/UpdateProfile';
+import Register from "./pages/forms/Register";
+import PostsPage from "./pages/posts-page/postsPage";
+import CreatePost from "./pages/create-post/CreatePost";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import UpdateProfile from "./pages/profile/UpdateProfile";
 import PostList from "./util/PostList.jsx";
 import PostCard from "./util/PostCard.jsx";
 import Footer from "./components/Footer/Footer";
 
-
 const URL = "http://localhost:8000/api/v1/";
 
 const App = () => {
-    const isAuthenticated = () => !!localStorage.getItem("token");
+  const isAuthenticated = () => !!localStorage.getItem("token");
 
-    const ProtectedRoute = ({ children }) => {
-        return isAuthenticated() ? children : <Navigate to="/login" replace />;
-    };
+  const ProtectedRoute = ({ children }) => {
+    return isAuthenticated() ? children : <Navigate to="/login" replace />;
+  };
 
   return (
-    
-      <Router>
+    <Router>
       <div className="min-h-screen flex flex-col">
         <Header />
         <Routes>
-        
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} /> 
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/posts" element={<PostsPage />} />
-          <Route path="/posts/create-post" element={<ProtectedRoute>
-                            <CreatePost />
-                        </ProtectedRoute>} />
-          <Route path="/admin-dashboard" element={<ProtectedRoute>
-                            <AdminDashboard />
-                        </ProtectedRoute>} /> 
-          
-         
-          <Route path="/profile" element={<ProtectedRoute>
-                            <Profile />
-                        </ProtectedRoute>} /> {/* Profile Page */}
-          
-        
-          <Route path="/profile/update" element={<ProtectedRoute>
-                            <UpdateProfile />
-                        </ProtectedRoute>} /> {/* Update Profile Page */}
+          <Route
+            path="/posts/create-post"
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />{" "}
+          {/* Profile Page */}
+          <Route
+            path="/profile/update"
+            element={
+              <ProtectedRoute>
+                <UpdateProfile />
+              </ProtectedRoute>
+            }
+          />{" "}
+          {/* Update Profile Page */}
+          <Route path="/" element={<PostList />} />
         </Routes>
         <Footer />
-        </div>
-      </Router>
-    );
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/redux/slices/postSlice.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Fetch posts from the API
+export const fetchPosts = createAsyncThunk(
+  "posts/fetchPosts",
+  async ({ page = 1, limit = 6 }, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/api/v1/posts?page=${page}&limit=${limit}`
+      );
+      return response.data; // Returns { posts, totalPosts, currentPage, totalPages }
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    totalPosts: 0,
+    currentPage: 1,
+    totalPages: 1,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.posts = action.payload.posts;
+        state.totalPosts = action.payload.total;
+        state.currentPage = Number(action.payload.page);
+        state.totalPages = action.payload.pages;
+        // console.log("action payload", action.payload);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to fetch posts";
+      });
+  },
+});
+
+const postReducer = postSlice.reducer;
+const postActions = postSlice.actions;
+
+export { postActions, postReducer };
+// export default postSlice.reducer;
